b90da87af2a04b0ae991d544637bfa3311f1343c
Dharati M <dharatim22@gmail.com>
2026-07-25 08:47:46 +0530

    Time: 0 ms (100%), Space: 55.8 MB (82.98%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.js
@@ -0,0 +1,18 @@
+var maxProduct = function(n) {
+    let max1 = 0;
+    let max2 = 0;
+
+    while (n > 0) {
+        let digit = n % 10;
+        n = Math.floor(n / 10);
+
+        if (digit > max1) {
+            max2 = max1;
+            max1 = digit;
+        } else if (digit > max2) {
+            max2 = digit;
+        }
+    }
+
+    return max1 * max2;
+};
