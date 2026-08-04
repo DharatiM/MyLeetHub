854358b7be252eb39b4801d9604cf03aed8c9772
Dharati M <dharatim22@gmail.com>
2026-08-04 09:07:59 +0530

    Time: 4 ms (40.94%), Space: 56.7 MB (95.28%) - LeetHub

---
+++ 3731-find-missing-elements/3731-find-missing-elements.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findMissingElements = function(nums) {
+    let min=Math.min(...nums)
+    let max=Math.max(...nums)
+        result=[]
+          let set=new Set(nums)
+
+    for(let i=min;i<=max;i++){
+        if(!set.has(i)){
+            result.push(i)
+        }
+    }
+    return result
+
+    
+};
